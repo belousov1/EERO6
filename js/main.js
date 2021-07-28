9b723f1908b38dca970aa6464a6846948ed1c6a1
belousov1 <66342147+belousov1@users.noreply.github.com>
2021-07-28 20:21:12 +0300

    EERO6

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:1500,
+		infinity:true,
+		easing:'ease',
+		
+	});
+
+
+});
+
+
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+	item.addEventListener("click", function () {
+		let currentBtn = item;
+		let tabId = currentBtn.getAttribute("data-tab");
+		let currentTab = document.querySelector(tabId);
+
+		if (!currentBtn.classList.contains('active')) {
+			tabsBtn.forEach(function (item) {
+				item.classList.remove('active');
+			});
+
+			tabsItems.forEach(function (item) {
+				item.classList.remove('active');
+			});
+
+			currentBtn.classList.add('active');
+			currentTab.classList.add('active');
+		}
+	});
+}
+
+document.querySelector('.tabs__nav-btn').click();
